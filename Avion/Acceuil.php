3adc3e3d1f701a9716052bf288339d3377fb6d57
VincentM3rin <astrucleandrin@gmail.com>
2025-05-14 11:51:29 +0200

    Add files via upload

---
+++ Avion/Acceuil.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<meta charset="utf8">
+		<title>Accueil</title>
+		<link  href="./avion.css" rel="stylesheet" />
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+	</head>
+	<body>
+        <nav>	
+			<br>
+			<div>
+				<a href="./index.html">Deconnexion</a>
+				<a href="./inscription.css">Inscription</a>
+			</div>
+		</nav>
+		<h1>Bienvenue ADMIN !</h1>
+		<h2>Choisisez un avion : </h2>
+		<a href="./Avion1.php">AirFrance</a>
+		<a href="./Avion2.html">Avion 2</a>
+		<a href="./Avion3.html">Avion 3</a>
+		<a href="./Avion4.html">Avion 4</a>
+		<a href="./Avion5.html">Avion 5</a>
+		<a href="./Avion6.html">Avion 6</a>
+</html>
